3b3197e87536feb57286b8c950935a4604bc43cb
twkim5235 <sssunggure@naver.com>
2021-07-11 22:56:30 +0900

    [FIX] (test)
    fix test source

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,10 +1,24 @@
 package hello.core.member;
 
+import hello.core.repository.MemberService;
+import hello.core.repository.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
-    @Test
-    void joing(){
 
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
     }
 }
