475303804e5f79bcb3982419f99148d1c0833079
twkim5235 <sssunggure@naver.com>
2021-07-11 22:52:04 +0900

    [ADD] (main, memberServiceTest)
    add main, memberServiceTest

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.repository.MemberService;
+import hello.core.repository.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args){
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMemver = " + findMember.getName());
+
+    }
+}
---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,10 @@
+package hello.core.member;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    @Test
+    void joing(){
+
+    }
+}
